26bd447c62b0ad8a4acc0981b65786132eeb4dd5
legit92 <nitindhake3@gmail.com>
2025-01-16 13:29:43 +0530

    completed m5

--- backend/App.js
+++ backend/App.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const connectDatabase = require('./db/Database');
 const ErrorHandler = require('./utils/errorhandler');
+const cookieParser = require('cookie-parser');
 const app = express()
+// const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 
 
 
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"))
+app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}))
 //config
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
@@ -13,6 +22,11 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
     })
 }
 
+//import router
+
+const user = require('./controller/user')
+
+app.use("./api/v2/user",user)
 
 connectDatabase();
 

ab85e3de68c3b4a79448103b00ca80e8090a6df6
legit92 <nitindhake3@gmail.com>
2025-01-25 16:18:59 +0530

    completed-m7

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,11 +1,22 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
+  const handelSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/api/v2/user/login",{email,password});
+      console.log(response.data);
+    } catch(error){
+      setError("Invalid Credentials!")
+      console.error("Login Error",error);
+    }
+  }
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -16,7 +27,7 @@ const Login = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6">
+            <form className="space-y-6" onSubmit={handelSubmit}>
                 <div>
                     <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
                     <div className="mt-1">
